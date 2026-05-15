e5d73e3e9529b7ed50c919201e9413df9c90e5fe
Mysztec <mysztecindustrial@gmail.com>
2026-05-14 21:17:50 -0300

    arrumei e subi pra vm

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'user',        // Usuário definido no Docker
-  password: 'password', // Senha definida no Docker
-  database: 'pharmaflow_db',
-  host: 'db',          // O nome do contêiner na rede do Docker
-  port: 5432,
+  user: process.env.DB_USER || 'user',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'pharmaflow_db',
+  host: process.env.DB_HOST || 'db', // No K8s, será o nome do Service do Postgres
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
